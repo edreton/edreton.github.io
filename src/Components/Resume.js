29f8a6802b96534f00bfd2abbd9f3fc1a169c91d
edreton <edgar.retontali@gmail.com>
2022-04-01 00:01:03 -0600

    build.minor

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -57,7 +57,7 @@ const Resume = ({ data }) => {
       <div className="row work">
         <div className="three columns header-col">
           <h1>
-            <span>Employment History</span>
+            <span>Employments</span>
           </h1>
         </div>
 
